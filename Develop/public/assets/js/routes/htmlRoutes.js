ece50e78dd47d222016c6953b88a69d036e067d5
Sabine-Hutter <Sabine.Hutter@ibm.com>
2020-10-14 20:55:17 -0700

    adding all base files

---
+++ Develop/public/assets/js/routes/htmlRoutes.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const path = require('path');
+
+
+
+
+module.exports = function (app) {
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, '../../index.html'));
+        console.log(__dirname)
+    })
+
+    app.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../../notes.html'));
+    })
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../../index.html'))
+    })
+}
